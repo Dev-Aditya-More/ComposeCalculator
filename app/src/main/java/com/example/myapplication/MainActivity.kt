3d982247163b06703062ed0742ec8e9dce0a3aa2
Aditya More <aditya1875more@gmail.com>
2025-04-26 15:23:45 +0530

    App done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,15 +11,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -31,6 +31,7 @@ import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -41,15 +42,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.red
-import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.airbnb.lottie.compose.*
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.ui.graphics.Color
+
+
 
 
 class MainActivity : ComponentActivity() {
@@ -88,15 +94,18 @@ fun MyCalciUI() {
             modifier = Modifier
                 .fillMaxSize()
                 .background(color = Color(212, 76, 76))
-                .padding(paddingValues)
+//                .padding(paddingValues)
+                .systemBarsPadding()
         )
         {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
                     .padding(paddingValues)
                     .padding(horizontal = 16.dp, vertical = 24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
 
             ) {
 
@@ -148,18 +157,37 @@ fun MyCalciUI() {
                             OutlinedTextField(
                                 value = firstNum,
                                 onValueChange = { firstNum = it },
-                                placeholder = { Text("Enter Number 1") },
-                                modifier = Modifier.weight(0.5f)
-                                    .background(color = Color.LightGray)
+                                placeholder = { Text(text = "Enter Number 1", color = Color.DarkGray)},
+                                modifier = Modifier.weight(0.5f),
+
+                                colors = OutlinedTextFieldDefaults.colors(
+                                    focusedBorderColor = Color.Green,
+                                    unfocusedBorderColor = Color.Red,
+                                    focusedTextColor = Color.Black,
+                                    unfocusedTextColor = Color.Black,
+                                    cursorColor = Color.Blue,
+                                    focusedPlaceholderColor = Color.Gray,
+                                    unfocusedPlaceholderColor = Color.Gray
+                                )
+
                             )
 
                             OutlinedTextField(
                                 value = secondNum,
                                 onValueChange = { secondNum = it },
-                                placeholder = { Text("Enter Number 2") },
+                                placeholder = { Text("Enter Number 2", color = Color.DarkGray) },
                                 modifier = Modifier
-                                    .weight(0.5f)
-                                    .background(color = Color.LightGray)
+                                    .weight(0.5f),
+
+                                colors = OutlinedTextFieldDefaults.colors(
+                                    focusedBorderColor = Color.Green,
+                                    unfocusedBorderColor = Color.Red,
+                                    focusedTextColor = Color.Black,
+                                    unfocusedTextColor = Color.Black,
+                                    cursorColor = Color.Blue,
+                                    focusedPlaceholderColor = Color.Gray,
+                                    unfocusedPlaceholderColor = Color.Gray
+                                )
                             )
 
                         }
@@ -191,7 +219,7 @@ fun MyCalciUI() {
                                 options.forEach { selectionOption ->
 
                                     DropdownMenuItem(
-                                        text = { Text(selectionOption) },
+                                        text = { Text(selectionOption)},
                                         onClick = {
                                             selectedOption = selectionOption
                                             expanded = false
@@ -227,6 +255,7 @@ fun MyCalciUI() {
 
                 Text(
                     "Result: $result",
+                    color = MaterialTheme.colorScheme.onBackground,
                     modifier = Modifier
                         .fillMaxWidth()
                         .wrapContentWidth(Alignment.CenterHorizontally)
